0380b3b80d4da86ab8b245197aef54a9ff748935
Kasper Pedersen <kasper.dharma@gmail.com>
2022-01-25 08:11:01 +0100

    feat:Questions are displayed on question vue

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -8,7 +8,9 @@ const router = useRouter();
 const store = useStore();
 
 const currentuser = computed(() => store.getters.getCurrentUser)
+const questions = computed(() => store.getters.getQuestions)
 
+console.log("hello")
 const goToResult = () => router.push('/result')
 
 
@@ -19,6 +21,11 @@ const goToResult = () => router.push('/result')
     <h2>Hello {{ currentuser.username }} welcome to the Questions</h2>
     <br>
     <h1>Your current score is {{ currentuser.currentscore }}</h1>
+    <ul id="questions">
+        <li v-for="item in questions.results" :key="item.question">
+            {{ item.question }}
+        </li>
+    </ul>
     <button @click="goToResult">Go To Result</button>
 </template>
 
